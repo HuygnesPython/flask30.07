a9e48974e860c22023fb0ae559e4067984185ec3
HuygnesPython <huygneszindocsgo@gmail.com>
2025-07-30 20:41:09 -0300

    Inicio

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/agendar')
+@app.route('/agendar', methods=['GET', 'POST'])
 def agendar():
-    return render_template('agendar.html')
+    if request.method == 'GET':
+        print('metodo GET utilizado')
+        return render_template('agendar.html')
+    elif request.method == 'POST':
+        nome = request.form.get('n')
+        email = request.form.get('e')
+        if nome == 'Huygnes' or 'huygnes':
+            if email == 'huygnes@gmail.com':
+                return render_template('segredo.html')
+        print('metodo POST utilizado', nome, email)
+        return render_template('agendar.html', nome = nome, email = email)
+    
 app.run(debug=True)
