7c307d50b43d26383af73ae769d89e7661a7a7a5
HuygnesPython <huygneszindocsgo@gmail.com>
2025-07-30 19:07:33 -0300

    Inicio

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
+@app.route('/agendar')
+def agendar():
+    return render_template('agendar.html')
 app.run(debug=True)
