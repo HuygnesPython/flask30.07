06ee9a19f43e23a2a308adeb1f2d1d04bb1d14e8
HuygnesPython <huygneszindocsgo@gmail.com>
2025-07-30 21:10:52 -0300

    Inicio

--- aula/app.py
+++ aula/app.py
@@ -12,10 +12,23 @@ def agendar():
     elif request.method == 'POST':
         nome = request.form.get('n')
         email = request.form.get('e')
+        telefone = request.form.get('num')
+        data = request.form.get('d')
+        hora = request.form.get('h')
+        servico = request.form.get('service')
+        
+        dados = {
+                'nome':nome, 
+                'email':email, 
+                'telefone':telefone, 
+                'data':data, 
+                'hora':hora, 
+                'servico':servico
+            }
+        
         if nome == 'Huygnes' or 'huygnes':
             if email == 'huygnes@gmail.com':
                 return render_template('segredo.html')
         print('metodo POST utilizado', nome, email)
-        return render_template('agendar.html', nome = nome, email = email)
-    
+        return render_template('ConfServico.html', dados = dados)
 app.run(debug=True)
